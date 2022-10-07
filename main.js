ae639e7e393e7c4d4498bee86456e1e193cf98f9
Daniel <daniel.de.s.sanches@gmail.com>
2022-10-06 21:36:43 -0300

    new push to main

---
+++ main.js
@@ -0,0 +1,40 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.has = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+    }
+}
+
+class Blockchain {
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Bloxk(0, "05/10/2022", "Genesis block", 0)
+    }
+
+    getLatesBlock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatesBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+let fesaCoin = Blockchain();
+fesaCoin.addBlock(new Block(1, "06/10/2021", {amount:4}));
+fesaCoin.addBlock(new Block(2, "07/10/2021", {amount:4}));
+
+console.log(JSON.stringify(fesaCoin, null,))
