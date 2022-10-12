88bf2867e7ea16edcbb8a3a56de751dc225abfdd
Daniel <daniel.de.s.sanches@gmail.com>
2022-10-12 12:38:43 -0300

    valid block

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
      calculateHash(){
          return SHA256(this.index + this.previousHash + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+     }
+
+     mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce ++;
+            this.hash = this.calculateHash();
+        } 
+        console.log("Block mined: " + this.hash);
      }
 }
 
@@ -32,9 +41,27 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++ ){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            
+            if(currentBlock.hash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 let fesaCoin = new Blockchain();
 fesaCoin.addBlock(new Block(1, "06/10/2021", {amount:4}));
 fesaCoin.addBlock(new Block(2, "07/10/2021", {amount:4}));
 
-console.log(JSON.stringify(fesaCoin, null, 5))
+console.log(JSON.stringify(fesaCoin, null, 4))
